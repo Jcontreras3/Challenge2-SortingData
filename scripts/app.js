1949c8de704ae05f385f954f67df3b0db1f37195
Jovann Contreras <jcontreras@codestack.co>
2022-01-25 15:42:02 -0800

    Added creating element

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,57 @@
+import {Create, InjectHere} from "./fetch.js"
+
 let GetId = document.getElementById('GetId');
 let First = document.getElementById('First');
 let Last = document.getElementById('Last');
 let GetEmail = document.getElementById('GetEmail');
 let GetHeight = document.getElementById('GetHeight');
 let GetAge = document.getElementById('GetAge');
+let Btn1 = document.getElementById('Btn1');
+let Btn2 = document.getElementById('Btn2');
+let Btn3 = document.getElementById('Btn3');
+let Btn4 = document.getElementById('Btn4');
+let Btn5 = document.getElementById('Btn5');
+let Btn6 = document.getElementById('Btn6');
+let Btn7 = document.getElementById('Btn7');
+let Btn8 = document.getElementById('Btn8');
+let Btn9 = document.getElementById('Btn9');
+let Btn10 = document.getElementById('Btn10');
+let Btn11 = document.getElementById('Btn11');
+let Btn12 = document.getElementById('Btn12');
+
+let People = [];
+ function getPeople(){
+    fetch("../data/data.json")
+    .then (response => response.json())
+    .then ( data => {
+        //console.log(data.People)
+        People = data.People;
+
+        for(let i = 0; i<People.length; i++){
+            Create(People[i]);
+        }
+    })
+}
+//getPeople()
+
+function PeopleID (){ 
+        fetch("../data/data.json")
+        .then((resp) => resp.json())
+        .then((data) =>{
+            People = data.People.reverse();
+           console.log(People)
+           for(let i = 0; i<People.length; i++){
+            Create(People[i]);
+        }
+       })
+    }
+    
+
+Btn1.addEventListener('click', () =>{
+    getPeople();
+    console.log("Hi")
+})
+
+Btn2.addEventListener('click', () =>{
+    PeopleID();
+})
--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,25 +1,63 @@
-let People = [];
- function getPeople(){
-    fetch("../data/data.json")
-    .then (response => response.json())
-    .then ( data => {
-        //console.log(data.People)
-        People = data.People;
-    })
+
+
+let InjectHere = document.getElementById('InjectHere');
+
+function Create(Idx){
+    let Id1 = document.createElement('li');
+    Id1.className = "list-group-item BgGold Maroon";
+    Id1.textContent = Idx.Id;
+    let col1 = document.createElement('div');
+    col1.className = "col-2";
+    col1.appendChild(Id1);
+
+    let Id2 = document.createElement('li');
+    Id2.className = "list-group-item BgGold Maroon";
+    Id2.textContent = Idx.FirstName;
+    let col2 = document.createElement('div');
+    col2.className = "col-2";
+    col2.appendChild(Id2);
+
+    let Id3 = document.createElement('li');
+    Id3.className = "list-group-item BgGold Maroon";
+    Id3.textContent = Idx.LastName;
+    let col3 = document.createElement('div');
+    col3.className = "col-2";
+    col3.appendChild(Id3);
+
+    let Id4 = document.createElement('li');
+    Id4.className = "list-group-item BgGold Maroon";
+    Id4.textContent = Idx.Email;
+    let col4 = document.createElement('div');
+    col4.className = "col-2";
+    col4.appendChild(Id4);
+
+    let Id5 = document.createElement('li');
+    Id5.className = "list-group-item BgGold Maroon";
+    Id5.textContent = Idx.Height;
+    let col5 = document.createElement('div');
+    col5.className = "col-2";
+    col5.appendChild(Id5);
+
+    let Id6 = document.createElement('li');
+    Id6.className = "list-group-item BgGold Maroon";
+    Id6.textContent = Idx.Age;
+    let col6 = document.createElement('div');
+    col6.className = "col-2";
+    col6.appendChild(Id6);
+    let Row = document.createElement('div');
+    Row.className = "row";
+    Row.appendChild(col1);
+    Row.appendChild(col2);
+    Row.appendChild(col3);
+    Row.appendChild(col4);
+    Row.appendChild(col5);
+    Row.appendChild(col6);
+    InjectHere.appendChild(Row);
+
+    
 }
-getPeople()
 
-function PeopleID (){ 
-   fetch("../data/data.json")
-   .then((resp) => resp.json())
-   .then((data) =>{
-       People = data.People.reverse();
-       console.log(People)
-   })
-}
-
-PeopleID();
-
+export{Create, InjectHere}
 // GetId.addEventListener('click', () =>{
 
 // });
